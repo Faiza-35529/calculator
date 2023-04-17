8121166917f1acd9ef415b2283629fb1e59f0207
Faiza-35529 <bsef20m518@pucit.edu.pk>
2023-04-18 01:08:35 +0500

    Calculator

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editTextNumber1, editTextNumber2;
     Button buttonPlus, buttonSub, buttonMul, buttonDiv;
     TextView textViewResult;
+    int num1, num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +21,17 @@ public class MainActivity extends AppCompatActivity {
 
         editTextNumber1 = findViewById(R.id.Number1);
         editTextNumber2 = findViewById(R.id.Number2);
+
         buttonPlus = findViewById(R.id.buttonAdd);
         buttonSub = findViewById(R.id.buttonSubtract);
         buttonMul = findViewById(R.id.buttonMultiply);
         buttonDiv = findViewById(R.id.buttonDivide);
+
         textViewResult = findViewById(R.id.result);
+
+        num1 = Integer.parseInt(editTextNumber1.getText().toString());
+        num2 = Integer.parseInt(editTextNumber2.getText().toString());
+
+
     }
 }
